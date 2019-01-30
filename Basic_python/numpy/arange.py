60cd570f0a105e3388fbf03c7296430a108a94f7
S108-HONDA\shiro <s183611u@st.go.tuat.ac.jp>
2019-01-30 11:42:33 +0900

    no message

---
+++ Basic_python/numpy/arange.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.arange(3)
+print(a)
+
+b = np.arange(3,6)
+print(b)
+
+print(a*b)
---
+++ Basic_python/numpy/meshgrid.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+X, Y = np.meshgrid(np.arange(0,10,1),np.arange(0,100,10))
+
+print(X,Y)
+print(X.ravel())
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot_wireframe(X,Y,np.sin(X+Y))
+plt.show()
